0b9ce16f2786f9bc6d759f6c26e6cec292eba05f
zouqimeng <155070498@qq.com>
2015-08-23 02:01:01 +0800

    log4net

---
+++ LdNetProjects/Test.Log/Program.cs
@@ -0,0 +1,46 @@
+﻿using log4net;
+using log4net.Config;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+[assembly: log4net.Config.XmlConfigurator(Watch = true)]
+namespace Test.Log
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Application.Run(new MainForm());
+            //创建日志记录组件实例
+            ILog log = log4net.LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+            //记录错误日志
+            log.Error("error", new Exception("发生了一个异常"));
+            //记录严重错误
+            log.Fatal("fatal", new Exception("发生了一个致命错误"));
+            //记录一般信息
+            log.Info("info000000000000000000000000000");
+            //记录调试信息
+            log.Debug("这是一个Debug日志");
+            //记录警告信息
+            log.Warn("warn");
+            Console.WriteLine("日志记录完毕。");
+            Console.Read();
+
+            Console.WriteLine("Begin Run");
+            XmlConfigurator.Configure();
+            Type type = MethodBase.GetCurrentMethod().DeclaringType;
+            ILog m_log = LogManager.GetLogger(type);
+            m_log.Debug("这是一个Debug日志");
+            m_log.Info("这是一个Info日志");
+            m_log.Warn("这是一个Warn日志");
+            m_log.Error("这是一个Error日志");
+            m_log.Fatal("这是一个Fatal日志");
+            Console.WriteLine("End");
+            Console.ReadLine();  
+        }
+    }
+}
